399701c0cb552c0932ba231b761d71db657b0185
Sovietmade <ch3rkas0v.yuri@gmail.com>
2013-10-18 12:37:58 +0400

    Update NetworkFunction.h

--- include/NeuralNetworks/NetworkFunction.h
+++ include/NeuralNetworks/NetworkFunction.h
@@ -12,35 +12,35 @@
 
 class NetworkFunction {
 public:
-NetworkFunction(){};
-virtual ~NetworkFunction(){};
-virtual double 	Process( double inParam ) = 0;
-virtual double 	Derivative( double inParam ) = 0;
+  NetworkFunction(){};
+  virtual ~NetworkFunction(){};
+  virtual double 	Process( double inParam ) = 0;
+  virtual double 	Derivative( double inParam ) = 0;
 };
 
 class Linear : public NetworkFunction {
 public:
-Linear(){};
-virtual ~Linear(){};
-virtual double 	Process( double inParam ){ return inParam; };
-virtual double 	Derivative( double inParam ){ return 0; };
+  Linear(){};
+  virtual ~Linear(){};
+  virtual double 	Process( double inParam ){ return inParam; };
+  virtual double 	Derivative( double inParam ){ return 0; };
 };
 
 
 class Sigmoid : public NetworkFunction {
 public:
-Sigmoid(){};
-virtual ~Sigmoid(){};
-virtual double 	Process( double inParam ){ return ( 1 / ( 1 + exp( -inParam ) ) ); };
-virtual double 	Derivative( double inParam ){ return ( this->Process(inParam)*(1 - this->Process(inParam)) );};
+  Sigmoid(){};
+  virtual ~Sigmoid(){};
+  virtual double 	Process( double inParam ){ return ( 1 / ( 1 + exp( -inParam ) ) ); };
+  virtual double 	Derivative( double inParam ){ return ( this->Process(inParam)*(1 - this->Process(inParam)) );};
 };
 
 class BipolarSigmoid : public NetworkFunction {
 public:
-BipolarSigmoid(){};
-virtual ~BipolarSigmoid(){};
-virtual double 	Process( double inParam ){ return ( 2 / ( 1 + exp( -inParam ) ) - 1 ) ;};
-virtual double 	Derivative( double inParam ){ return ( 0.5 * ( 1 + this->Process( inParam ) ) * ( 1 - this->Process( inParam ) ) ); };
+  BipolarSigmoid(){};
+  virtual ~BipolarSigmoid(){};
+  virtual double 	Process( double inParam ){ return ( 2 / ( 1 + exp( -inParam ) ) - 1 ) ;};
+  virtual double 	Derivative( double inParam ){ return ( 0.5 * ( 1 + this->Process( inParam ) ) * ( 1 - this->Process( inParam ) ) ); };
 };
 
 #endif /* NETWORKFUNCTION_H_ */
